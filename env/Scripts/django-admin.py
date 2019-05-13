9f4ba27c2cab7d9d8e67337990e12190c8a50c50
Eduardo Silva <edlusilvaca@ittepic.edu.mx>
2019-05-13 12:37:12 -0600

    Primer commit tutorial 4

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\eduard~1\docume~1\python-virtual-environments\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
